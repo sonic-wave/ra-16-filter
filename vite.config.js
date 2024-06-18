c18093d83cd9b08fecc884d7419cd6170148a4c4
Ivan Khokhlov <mozhno@MacBook-Air-Mozhno.local>
2024-06-18 13:45:20 +0300

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ra-16-filters",
+  base: "/ra-16-filter",
   plugins: [react()],
 })
